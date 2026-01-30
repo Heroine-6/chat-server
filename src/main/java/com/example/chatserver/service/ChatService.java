0639ab6b16cdc43f2bd79d39aa5b0dc2fcbec85c
i-am-dahee <hxxly02@gmail.com>
2026-01-30 10:54:45 +0900

    refactor: 필요 없는 코드 제거 #5

--- src/main/java/com/example/chatserver/service/ChatService.java
+++ src/main/java/com/example/chatserver/service/ChatService.java
@@ -19,8 +19,6 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 @Service
