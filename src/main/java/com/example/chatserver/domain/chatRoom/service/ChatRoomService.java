1e10d952b4fffefd935a423d3c09b998c9159b5f
i-am-dahee <hxxly02@gmail.com>
2026-02-03 11:33:17 +0900

    refactor: 필요 없는 코드 제거 #13

--- src/main/java/com/example/chatserver/domain/chatRoom/service/ChatRoomService.java
+++ src/main/java/com/example/chatserver/domain/chatRoom/service/ChatRoomService.java
@@ -7,7 +7,6 @@ import com.example.chatserver.domain.chatRoom.dto.response.GetMyRoomsResponse;
 import com.example.chatserver.domain.chatRoom.dto.response.FindRoomResponse;
 import com.example.chatserver.domain.chatRoom.repository.ChatRoomRepository;
 import com.example.chatserver.domain.chatMessage.repository.ReadStateRepository;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
